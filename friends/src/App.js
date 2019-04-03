2b08cf7e7e9a4a1cd63a83797257f8833b8aef3a
Brandon_J_Allison <you@example.com>
2019-04-03 09:48:06 -0400

    friends list component returning friendslist

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FriendsList from './FriendsList'
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      friendsList: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+    .get(`http://localhost:5000/friends`)
+    .then(response => {
+      this.setState(() => ({ friendsList: response.data }));
+      console.log(response)
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+    
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <FriendsList data={this.state.friendsList} />
       </div>
     );
   }
---
+++ friends/src/FriendsList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FriendsList = props => (
+    <div className='friendsList'>
+        {props.data.map(friend => (
+            <div key={friend.id}>
+                <p><span>Name:</span>{friend.name}</p>
+                <p><span>Age:</span>{friend.age}</p>
+                <p><span>Email:</span>{friend.email}</p>
+            </div>
+        ))}
+    </div>
+);
+
+export default FriendsList;
+
+//id: 1, name: "Ben", age: 30, email: "ben@lambdaschool.com"
